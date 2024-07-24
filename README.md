abbf1a7c7378e888ed69f5e5182b3c291da48724
Richard Dallaway <richard@dallaway.com>
2024-07-24 22:19:02 +0100

    intro

---
+++ README.md
@@ -0,0 +1,8 @@
+Working through [Server-Driven Web Apps with htmx](https://pragprog.com/titles/mvhtmx/server-driven-web-apps-with-htmx/) using Rust
+
+```
+cargo watch -c -x run
+```
+
+Then browse to <localhost:3000>
+
---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use axum::{
+    body::Body,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use tower::ServiceExt;
+use tower_http::services::{ServeDir, ServeFile};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let assets = Router::new().nest_service("/assets", ServeDir::new("assets"));
+
+    let app = Router::new()
+        .route("/", get(root))
+        .merge(assets)
+        .route("/version", get(version));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root(req: axum::http::Request<Body>) -> Response {
+    ServeFile::new("assets/index.html")
+        .oneshot(req)
+        .await
+        .into_response()
+}
+
+async fn version() -> &'static str {
+    env!("CARGO_PKG_VERSION")
+}
